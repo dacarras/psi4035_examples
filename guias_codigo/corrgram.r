3b2b840d8d6e260061586d38eba3885d72be2bed
dacarras <dacarras@gmail.com>
2022-04-14 11:09:02 -0400

    add files taller 05

---
+++ guias_codigo/corrgram.r
@@ -0,0 +1,10 @@
+pairs(                                 # nombre de la funcion
+   ~wt+mpg+disp+cyl,                   # argumento de formula
+   data = mtcars,                      # objeto de datos
+   main = "Scatterplot Matrix"         # titulo
+   )
+
+
+
+pairs(mtcars)
+
