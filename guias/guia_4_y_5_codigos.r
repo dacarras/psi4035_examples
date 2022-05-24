a26ddb9fa0c065d907da1fd3cf30f78ceafe2f02
dacarras <dacarras@gmail.com>
2022-05-23 20:13:13 -0400

    update

--- guias/guia_4_y_5_codigos.r
+++ guias/guia_4_y_5_codigos.r
@@ -1,10 +1,3 @@
-# apertura de datos
-data_crime <- read.csv('/Users/d/Dropbox (Personal)/_git/psi4035_examples/guias/faculty.csv'
-  url(
-    'https://raw.githubusercontent.com/dacarras/psi4035_examples/master/data/datos_fl_crime.csv'
-  )
-)
-
 #----------------------------------------------------------
 # apertura de datos
 #----------------------------------------------------------
@@ -21,34 +14,89 @@ dplyr::glimpse(data_salary)
 # preparar datos
 #----------------------------------------------------------
 
+library(dplyr)
+data_model <- data_salary %>%
+              mutate(full = case_when(
+                rank == 1 ~ 0, # profesor asistente
+                rank == 2 ~ 0, # profesor asociado
+                rank == 3 ~ 1  # profesor titular
+                )) %>%
+              mutate(id_j = 1) %>%
+              mutate(market_g = r4sda::c_mean(market, id_j)) %>%
+              mutate(market_m = market - market_g) %>%
+              mutate(male_g   = r4sda::c_mean(male, id_j)) %>%
+              mutate(male_m   = male - male_g) %>%
+              dplyr::glimpse()
 
+#----------------------------------------------------------
+# cantidad de casos por dummy
+#----------------------------------------------------------
+
+dplyr::count(data_model, market)
+dplyr::count(data_model, yearsdg)
+dplyr::count(data_model, male)
+dplyr::count(data_model, full)
 
 #----------------------------------------------------------
 # modelos
 #----------------------------------------------------------
 
-f01 <- as.formula(salary ~ market + yearsdg + male + rank)
+f01 <- as.formula(salary ~ market + yearsdg)
 
+f02 <- as.formula(salary ~ market + yearsdg + market:yearsdg)
+
+f03 <- as.formula(salary ~ market + male)
+f04 <- as.formula(salary ~ market + male + market:male)
+
+f05 <- as.formula(salary ~ market + male + market:male + full + full:male)
+
+f06 <- as.formula(salary ~ market_m + male_m)
+f07 <- as.formula(salary ~ market_m + male_m + market_m:male_m)
+
+f08 <- as.formula(salary ~ market_m + male)
+f09 <- as.formula(salary ~ market_m + male + market_m:male)
 
 #----------------------------------------------------------
 # ajustar modelos
 #----------------------------------------------------------
 
-m01 <- lm(f01, data = data_salary)
+m01 <- lm(f01, data = data_model)
+m02 <- lm(f02, data = data_model)
+m03 <- lm(f03, data = data_model)
+m04 <- lm(f04, data = data_model)
+m05 <- lm(f05, data = data_model)
+m06 <- lm(f06, data = data_model)
+m07 <- lm(f07, data = data_model)
+m08 <- lm(f08, data = data_model)
+m09 <- lm(f09, data = data_model)
+
 
 #----------------------------------------------------------
 # mostrar estimados
 #----------------------------------------------------------
 
+texreg::screenreg(
+    list(m01, m02, m03, m04, m05, m06, m07),
+    star.symbol = "*", 
+    center = TRUE, 
+    doctype = FALSE,
+    dcolumn = TRUE, 
+    booktabs = TRUE,
+    single.row = FALSE,
+    include.loglik = TRUE
+    )
 
+#----------------------------------------------------------
+# mostrar estimados
+#----------------------------------------------------------
 
-## dicotomizar variable con if_else (dplyr)
-data_crime <- data_crime %>%
-              mutate(dummy_i = dplyr::if_else(I > mean(I),1,0)) %>%
-              mutate(dummy_h = dplyr::if_else(HS > mean(HS),1,0)) %>%
-              mutate(dummy_u = dplyr::if_else(U > mean(U),1,0)) %>%
-              dplyr::glimpse()
+texreg::screenreg(
+    list(m03, m04, m06, m07, m08, m09),
+    star.symbol = "*", 
+    center = TRUE, 
+    doctype = FALSE,
+    dcolumn = TRUE, 
+    booktabs = TRUE,
+    single.row = FALSE
+    )
 
-
-## mostrar todos los datos
-knitr::kable(data_crime)
